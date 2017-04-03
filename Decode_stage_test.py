5bd8d54bb7c543cda146a31457af3245846dea38
Trey Franklin <franklin.trey@gmail.com>
2017-04-03 11:28:17 -0500

    Updated for new format of Decode's receive_instruction method

--- Decode_stage_test.py
+++ Decode_stage_test.py
@@ -15,25 +15,25 @@ class MyTestCase(unittest.TestCase):
 
     def test_receive_instruction(self):
         decode = Decode(self.register_file, Execute())
-        decode.receive_instruction(Instruction('Add', "$t1", "$t2", "t3"))
+        decode.receive_instruction(Instruction('Add', "$t1", "$t2", "t3"), 8)
         self.assertEqual("01001", decode.write_register)
         self.assertEqual("01010", decode.read_reg_1)
         self.assertEqual("01011", decode.read_reg_2)
 
     def test_write_to_write_reg(self):
         decode = Decode(self.register_file, Execute())
-        decode.receive_instruction(Instruction('Add', "$t1", "$t2", "t3"))
+        decode.receive_instruction(Instruction('Add', "$t1", "$t2", "t3"), 8)
         decode.write_to_register("Test data.")
         self.assertEqual("Test data.", decode.register_file[9])
 
     def test_extend_immediate(self):
         decode = Decode(self.register_file, Execute())
-        decode.receive_instruction(Instruction('addi', '$t8', '$zero', '1'))
+        decode.receive_instruction(Instruction('addi', '$t8', '$zero', '1'), 8)
         self.assertEqual("00000000000000000000000000000001", decode.sign_extended_immediate)
 
     def test_extend_immediate_neg(self):
         decode = Decode(self.register_file, Execute())
-        decode.receive_instruction(Instruction('addi', '$t8', '$zero', '-10'))
+        decode.receive_instruction(Instruction('addi', '$t8', '$zero', '-10'), 8)
         self.assertEqual("11111111111111111111111111110110", decode.sign_extended_immediate)
 
 
